47500eb422c3d374984ef2ea19627dfe3141458e
Denis <dnnshlgn@icloud.com>
2022-10-02 15:21:48 +0300

    pr27

---
+++ pr27/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 27:
+// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите число  :" );
+int n = Convert.ToInt32(Console.ReadLine());
+int i = 0;
+int res = 0;
+while ( i <= n)
+{
+   res = (n%10) + res;
+   n = n/10;
+   i++;
+}
+Console.WriteLine(res);
